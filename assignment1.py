032c5d7240699b779650d7d1fbd7d988f2fe46f5
Ron Kang <ronjinhong.kang@my.jcu.edu.au>
2018-12-03 17:18:15 +0800

    test 2

--- assignment1.py
+++ assignment1.py
@@ -6,6 +6,7 @@ Replace the contents of this module docstring with your own details.
 def main():
     print("Songs To Learn 1.0 - by <Your Name>")
     print("test")
+#     hello
 
 
 if __name__ == '__main__':
