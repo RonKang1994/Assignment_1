16f7b64d1860b57b5cfadd4ccf858d2cd995556d
Ron Kang <ronjinhong.kang@my.jcu.edu.au>
2018-12-07 17:28:37 +0800

    Added function for learning songs

--- assignment1.py
+++ assignment1.py
@@ -3,44 +3,68 @@ CP 1404 Assignment 1 Ron Kang 13668385
 """
 import csv
 
+def main_menu(song_num):
+    """Main Menu"""
+    print("{} songs loaded".format(song_num))
+    print("Menu:\nL - List Songs\nA - Add new song\nC - Complete a song\nQ - Quit")
 
-def menu(song_num, menu_type, song_list):
-    if menu_type == 1:
-        """Main Menu"""
-        print("{} songs loaded".format(song_num))
-        print("Menu:\nL - List Songs\nC - Complete a song\nQ - Quit")
-    elif menu_type == 2:
-        """Print out the whole list
-            row_count - used to check how many songs there are
-            song_learn - Used to check how many songs are learned
-            song_not_learn - USed to check how many songs are not learned
-        """
-        row_count = 1
-        song_learn = 0
-        song_not_learn = 0
-        for row in song_list:
-            if row[-1] == 'n':
-                row[-1] = '*'
-                song_not_learn += 1
-            else:
-                row[-1] = ' '
-                song_learn += 1
-            print("{}. {} {:<30} - {:<30} ({})".format(row_count, row[-1], row[0], row[1], row[2]))
-            row_count += 1
-        if song_not_learn == 0:
-            print("No more songs to learn")
+
+def full_song_list(check_list):
+    """Print out the whole list
+        row_count - used to check how many songs there are
+        song_learn - Used to check how many songs are learned
+        song_not_learn - USed to check how many songs are not learned
+    """
+    row_count = 1
+    song_learn = 0
+    song_not_learn = 0
+    for row in check_list:
+        if row[-1] == 'n':
+            row[-1] = '*'
+            song_not_learn += 1
         else:
-            print("{} songs learned, {} songs still to learn".format(song_learn, song_not_learn))
-        print(song_list)
+            row[-1] = ' '
+            song_learn += 1
+        print("{}. {} {:<30} - {:<30} ({})".format(row_count, row[-1], row[0], row[1], row[2]))
+        if row[-1] == '*':
+            row[-1] = 'n'
+        else:
+            row[-1] = 'y'
+        row_count += 1
+    if song_not_learn == 0:
+        print("No more songs to learn")
+    else:
+        print("{} songs learned, {} songs still to learn".format(song_learn, song_not_learn))
+
+
+def invalid_input_number(input):
+    if input <= 0:
+        print("Number must be > 0")
+        return False
+    else:
+        return True
+
+
+def learn_song(song_list):
+    """Used for learning songs
+    song_choice - which song to learn
+    """
+    song_choice = 0
+    song_choice = int(input("Enter the number of a song to mark as learned\n"))
+    while(invalid_input_number(song_choice) == False):
+        song_choice = int(input("Enter the number of a song to mark as learned\n"))
+
+    return song_choice
+
 
 def main():
     """
-    menu_type - Used to check which menu to show
     song_num - Total number of songs
     choice_menu - user menu choice
+    song_to_learn - Which song on the list to convert from n to y
     """
-    menu_type = 1
     song_num = 0
+    song_to_learn = 0
     choice_menu = ""
     song_list = []
     song_file = open('songs.csv', 'r')
@@ -54,13 +78,18 @@ def main():
     print("Songs To Learn 1.0 - by Ron Kang")
 
     while(choice_menu.upper() != "Q"):
-        menu(song_num, menu_type, song_list)
+        main_menu(song_num)
+        print(song_list)
         choice_menu = str(input(">>> "))
         if choice_menu.upper() == "L":
-            menu_type = 2
+            full_song_list(song_list)
+            print(song_list)
         elif choice_menu.upper() == "A":
             print("Test A Choice")
         elif choice_menu.upper() == "C":
+            song_to_learn = learn_song(song_list)
+            song_list[song_to_learn - 1][-1] = 'y'
+            print("{} by {} learned".format(song_list[song_to_learn - 1][0],song_list[song_to_learn - 1][1]))
             print("Test C Choice")
 
 
